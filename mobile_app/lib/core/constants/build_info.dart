427b00e21bec2a4b4cade0de42a3f3669e1e56d0
Metin <oez@me.com>
2026-04-04 04:24:54 +0200

    chore: update mobile build stamp 04.04.2026 04:24

--- mobile_app/lib/core/constants/build_info.dart
+++ mobile_app/lib/core/constants/build_info.dart
@@ -1,3 +1,3 @@
 class BuildInfo {
-  static const String buildTime = String.fromEnvironment('APP_BUILD_TIME', defaultValue: '03.04.2026 22:26');
+  static const String buildTime = String.fromEnvironment('APP_BUILD_TIME', defaultValue: '04.04.2026 04:17');
 }
