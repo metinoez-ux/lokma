9924f8e72e03ea557479da595de9f9137355583b
Metin <oez@me.com>
2026-04-13 09:17:32 +0200

    chore(versioning): update version stamp

--- mobile_app/lib/core/constants/build_info.dart
+++ mobile_app/lib/core/constants/build_info.dart
@@ -1,4 +1,4 @@
 class BuildInfo {
-  static const String buildTime = '13.04.2026 08:06'; // Auto-updated via Version Stamp
+  static const String buildTime = '13.04.2026 09:17'; // Auto-updated via Version Stamp
   static const String buildVersion = '1.0.0-beta.8';
 }
