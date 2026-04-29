808b17020e953d4b5acd132e0114d261b01f556a
Metin <oez@me.com>
2026-04-29 12:25:13 +0200

    chore: remove debug scripts to clear GitGuardian alert

--- admin_portal/src/app/api/check_notif.js
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin');
-
-// Next.js projesinde FIREBASE_SERVICE_ACCOUNT_KEY env var olabilir.
-// Veya lib/firebase-admin.ts nasil calisiyorsa.
-// Bir deneme yapalim. 
-// Veya dogrudan admin portal icinde oldugumuzdan, eger .env.local varsa oradan alabiliriz.
